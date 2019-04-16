80635ad59a2c112cdb4ffe056ddc53a6c27884de
bariudin777 <45856254+bariudin777@users.noreply.github.com>
2019-04-16 23:06:01 +0300

    trying to add this fucking files  i hate it..

---
+++ CommandClient.cs
@@ -0,0 +1,58 @@
+﻿using FlightSimulator.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.Model
+{
+    class CommandClient : ICommandClient
+    {
+        private int port;
+        private string ip;
+        private IPEndPoint endPoint;
+        private TcpClient tcpClient;
+
+        // here i init the members for the connection
+        public CommandClient(int port, string ip)
+        {
+            this.port = port;
+            this.ip = ip;
+        }
+        
+        // Connect method:
+        // First i get the ip address
+        // After this i connect by TCP protocol
+        
+        public void connect()
+        {
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            endPoint = new IPEndPoint(ipAddress, port);
+            tcpClient = new TcpClient();
+            tcpClient.Connect(endPoint);
+            Console.WriteLine("Connected");
+        }
+        //Dissconnect as TCP client
+        public void disconnect()
+        {
+            tcpClient.Close();
+        }
+        // Write the massage to the simulator
+        // Use StreamWrite to write the simuletur
+        public void writeMsg(string msg)
+        {
+
+            NetworkStream ns = tcpClient.GetStream();
+            StreamWriter st = new StreamWriter(ns);
+            st.Write(msg);
+            st.Flush();// very important
+
+
+        }
+    }
+}
---
+++ ICommandClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.Model.Interface
+{
+    interface ICommandClient
+    {
+        // the methods that i will use
+        void connect();
+        void disconnect();
+        void writeMsg(string msg);
+        
+
+    }
+}
